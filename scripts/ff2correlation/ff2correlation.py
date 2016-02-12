a8fd36c238a55718b5f091134276d52ef435a831
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-02-12 12:48:35 +0000

    Can plot in 3d

--- scripts/ff2correlation/ff2correlation.py
+++ scripts/ff2correlation/ff2correlation.py
@@ -7,33 +7,37 @@ import cst
 
 if __name__ == "__main__":
     # CST Example ##############################################################
-    # M1 = cst.loadfile("cst_870_top.txt")
-    # M2 = cst.loadfile("cst_870_side.txt")
+    M1 = cst.loadfile("cst_870_top.txt")
+    M2 = cst.loadfile("cst_870_side.txt")
 
-    # Ft1 = cst.ff2mat(M1, 3, 4)
-    # Fp1 = cst.ff2mat(M1, 5, 6)
+    Ft1 = cst.ff2mat(M1, 3, 4)
+    Fp1 = cst.ff2mat(M1, 5, 6)
 
-    # Ft2 = cst.ff2mat(M2, 3, 4)
-    # Fp2 = cst.ff2mat(M2, 5, 6)
+    Ft2 = cst.ff2mat(M2, 3, 4)
+    Fp2 = cst.ff2mat(M2, 5, 6)
 
-    # ECC = l3d.ecc(Ft1, Ft2, Fp1, Fp2)
-    # print(ECC)
+    ECC = l3d.ecc(Ft1, Ft2, Fp1, Fp2)
     ############################################################################
 
     # SATIMO Example ###########################################################
-    M1 = satimo.loadfile("satimo_ifa.txt")
-    M2 = satimo.loadfile("satimo_slot.txt")
+    # M1 = satimo.loadfile("satimo_ifa.txt")
+    # M2 = satimo.loadfile("satimo_slot.txt")
 
-    Ft1 = satimo.ff2mat(M1, 4, 5)
-    Fp1 = satimo.ff2mat(M1, 2, 3)
-    Ft2 = satimo.ff2mat(M2, 4, 5)
-    Fp2 = satimo.ff2mat(M2, 2, 3)
+    # Ft1 = satimo.ff2mat(M1, 4, 5)
+    # Fp1 = satimo.ff2mat(M1, 2, 3)
+    # Ft2 = satimo.ff2mat(M2, 4, 5)
+    # Fp2 = satimo.ff2mat(M2, 2, 3)
 
-    ECC = l3d.ecc(Ft1, Ft2, Fp1, Fp2)
-
-    Etot = sqrt(abs(Ft2)**2 + abs(Fp2)**2)
-    l3d.plotflat(Etot)
-    show()
-    print(ECC)
+    # ECC = l3d.ecc(Ft1, Ft2, Fp1, Fp2)
     ############################################################################
+    figure(1)
+    Etot = sqrt(abs(Ft1)**2 + abs(Fp1)**2)
+    l3d.plot3d(Etot, 10)
+
+    figure(2)
+    Etot = sqrt(abs(Ft2)**2 + abs(Fp2)**2)
+    l3d.plot3d(Etot, 10)
+    show(False)
+    print(ECC)
+    input()
 
