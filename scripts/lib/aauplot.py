f96dced25572f284cd9412c781433e4d66009729
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-03-10 12:03:02 +0000

    123

--- scripts/lib/aauplot.py
+++ scripts/lib/aauplot.py
@@ -46,20 +46,33 @@ def to_db(x):
 # @param label Label for the legend
 def sparam(f, s, c="-", label=""):
     f *= freqscale(f)
-    plt.xlim(f.min(), f.max())
+    plt.plot(f, s, c, label=label)
+
+
+# Finish the S-parameter plot with legend, etc.
+#
+# @param kwargs Parameters passed onto matplotlib.pyplot.legend().
+def end_sparam(**kwargs):
+    if kwargs.get("loc") == None:
+        kwargs["loc"] = 1
+    if kwargs.get("fontsize") == None:
+        kwargs["fontsize"] = 8
+    if kwargs.get("handlelength") == None:
+        kwargs["handlelength"] = 3
+
     plt.ylim(-24, 0)
     plt.yticks([-24, -18, -12, -6, 0])
-    plt.plot(f, s, c, label=label)
 
     plt.xlabel("Frequency [MHz]")
     plt.ylabel("Magnitude [dB]")
 
-    # TODO: Do not plot all these every time a new graph is added.
     for x in [700, 960, 1710, 2650]:
         plt.axvline(x, color='k', linestyle='--')
         plt.text(x, 0.7, x, ha='center', va='bottom', bbox=dict(fc='white', ec='none', pad=0))
 
     plt.grid(True)
+    plt.legend(**kwargs)
+    plt.tight_layout()
 
 # Plot an efficiency graph.
 #
@@ -83,12 +96,12 @@ def efficiency(f, e, c="-", label=""):
 
 # Finish the efficiency plot with legend, etc.
 #
-# @param f Frequency axis.
-def end_efficiency(f, loc=1, fontsize=8):
-    plt.legend(loc=loc, fontsize=fontsize)
+# @param loc Location of the legend (like matplotlib.pyplot.legend())
+# @param fontsize Font size for the legend.
+def end_efficiency(loc=1, fontsize=8):
+    plt.legend(loc=loc, fontsize=fontsize, handlelength=3)
     plt.xlabel("Frequency [MHz]")
     plt.ylabel("Efficiency [dB]")
-    plt.xlim(f.min(), f.max())
     plt.ylim(-15, 0)
     plt.yticks([-15,-12, -9, -6, -3, 0])
 
@@ -106,18 +119,4 @@ def end_efficiency(f, loc=1, fontsize=8):
     ax2.set_yticklabels([int(x) for x in np.around(100*10**(ax2.get_yticks()/10))])
     ax2.set_ylabel("Efficiency [%]", rotation=270, va="bottom")
 
-    plt.tight_layout()
-
-
-
-# Add a legend to the plot. The labels for the legend are specified with the
-# "label" option when plotting a graph.
-#
-# @param kwargs All parameters are passed on to matplotlib.pyplot.legend().
-def legend(**kwargs):
-    if kwargs.get("loc") == None:
-        kwargs["loc"] = 1
-    if kwargs.get("fontsize") == None:
-        kwargs["fontsize"] = 8
-    plt.legend(**kwargs)
     plt.tight_layout()
--- scripts/lib/doc/AauPlotLib/examples/ex1.py
+++ scripts/lib/doc/AauPlotLib/examples/ex1.py
@@ -5,10 +5,10 @@ import aauplot
 aauplot.figure()
 m = loadtxt("ex1_sparams.csv", skiprows=3).T
 
-aauplot.sparam(m[0], m[1], "S11")
-aauplot.sparam(m[0], m[3], "S21")
-aauplot.sparam(m[0], m[5], "S22")
-aauplot.legend(loc=1)
+aauplot.sparam(m[0], m[1], label="S11")
+aauplot.sparam(m[0], m[3], label="S21")
+aauplot.sparam(m[0], m[5], label="S22")
+aauplot.end_sparam(loc=1)
 
 savefig("ex1_sparams.pdf")
 show()
