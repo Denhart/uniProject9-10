e47d85002babe45bbc0db927bc91105152ebf792
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-03-10 10:09:32 +0000

    123

--- mockups/001-trianglefeed/measurements/2016-02-26-sparameters_and_efficiency/analyze.py
+++ mockups/001-trianglefeed/measurements/2016-02-26-sparameters_and_efficiency/analyze.py
@@ -2,20 +2,28 @@ from numpy import *
 from matplotlib.pyplot import *
 import aauplot
 
-m = loadtxt("sparams.csv", skiprows=3).T
 aauplot.figure()
-aauplot.sparam(m[0], m[1], "S11")
-aauplot.sparam(m[0], m[3], "S21")
-aauplot.sparam(m[0], m[5], "S22")
-aauplot.legend()
+m = loadtxt("sim/top_09pf.txt", skiprows=2).T
+aauplot.sparam(m[0], m[1], "--b", label="S11 sim")
+m = loadtxt("sim/s21_09pf_03pf.txt", skiprows=2).T
+aauplot.sparam(m[0], m[1], "--g", label="S21 sim")
+m = loadtxt("sim/side_03pf.txt", skiprows=2).T
+aauplot.sparam(m[0], m[1], "--r", label="S22 sim")
+
+m = loadtxt("sparams.csv", skiprows=3).T
+aauplot.sparam(m[0], m[1], "-b", label="S11")
+aauplot.sparam(m[0], m[3], "-g", label="S21")
+aauplot.sparam(m[0], m[5], "-r", label="S22")
+
+aauplot.legend(ncol=2, loc=0)
 savefig("sparams.pdf")
 
 m2 = loadtxt("../2016-02-25-sparameters_good_vna/Csh1_09_Csh2_11.csv", skiprows=3).T
 aauplot.figure()
-aauplot.sparam(m[0]  , m[1]  , "$S_{11}$, $C_{\mathrm{sh}2} =$ 0.3 pF")
-aauplot.sparam(m2[0] , m2[1] , "$S_{11}$, $C_{\mathrm{sh}2} =$ 0.9 pF")
-aauplot.sparam(m[0]  , m[5]  , "$S_{22}$, $C_{\mathrm{sh}2} =$ 0.3 pF")
-aauplot.sparam(m2[0] , m2[5] , "$S_{22}$, $C_{\mathrm{sh}2} =$ 0.9 pF")
+aauplot.sparam(m[0]  , m[1]  , label="$S_{11}$, $C_{\mathrm{sh}2} =$ 0.3 pF")
+aauplot.sparam(m2[0] , m2[1] , label="$S_{11}$, $C_{\mathrm{sh}2} =$ 0.9 pF")
+aauplot.sparam(m[0]  , m[5]  , label="$S_{22}$, $C_{\mathrm{sh}2} =$ 0.3 pF")
+aauplot.sparam(m2[0] , m2[5] , label="$S_{22}$, $C_{\mathrm{sh}2} =$ 0.9 pF")
 aauplot.legend(loc=4)
 savefig("sparam_comparison.pdf")
 
