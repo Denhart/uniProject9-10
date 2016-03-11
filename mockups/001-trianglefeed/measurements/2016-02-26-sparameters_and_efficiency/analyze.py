9a16ff9218b9bc7531011e6653a84901cba2c596
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-03-11 12:20:27 +0000

    Wrote about mockup

--- mockups/001-trianglefeed/measurements/2016-02-26-sparameters_and_efficiency/analyze.py
+++ mockups/001-trianglefeed/measurements/2016-02-26-sparameters_and_efficiency/analyze.py
@@ -3,17 +3,19 @@ from matplotlib.pyplot import *
 import aauplot
 
 aauplot.figure()
+# Measurements
+m = loadtxt("sparams.csv", skiprows=3).T
+aauplot.sparam(m[0], m[1], "-b", label="S11 meas")
+aauplot.sparam(m[0], m[5], "-g", label="S22 meas")
+aauplot.sparam(m[0], m[3], "-r", label="S21 meas")
+
+# Simulations
 m = loadtxt("sim/top_09pf.txt", skiprows=2).T
 aauplot.sparam(m[0], m[1], "--b", label="S11 sim")
-m = loadtxt("sim/s21_09pf_03pf.txt", skiprows=2).T
-aauplot.sparam(m[0], m[1], "--g", label="S21 sim")
 m = loadtxt("sim/side_03pf.txt", skiprows=2).T
-aauplot.sparam(m[0], m[1], "--r", label="S22 sim")
-
-m = loadtxt("sparams.csv", skiprows=3).T
-aauplot.sparam(m[0], m[1], "-b", label="S11")
-aauplot.sparam(m[0], m[3], "-g", label="S21")
-aauplot.sparam(m[0], m[5], "-r", label="S22")
+aauplot.sparam(m[0], m[1], "--g", label="S22 sim")
+m = loadtxt("sim/s21_09pf_03pf.txt", skiprows=2).T
+aauplot.sparam(m[0], m[1], "--r", label="S21 sim")
 
 aauplot.end_sparam(ncol=2, loc=8)
 savefig("sparams.pdf")
---
+++ mockups/001-trianglefeed/measurements/2016-03-08-triangle_both_efficiency/analyze_ecc.py
@@ -0,0 +1,33 @@
+import satimo
+import aauplot
+from numpy import *
+from matplotlib.pyplot import *
+
+# Low band
+f1,h1,v1 = satimo.loadtrx("triag_top_horiz_lb.trx")
+f2,h2,v2 = satimo.loadtrx("triag_side_horiz_lb.trx")
+
+f_lb = f1
+ecc_lb = satimo.ecc(h1,h2,v1,v2)
+
+# High band
+f1,h1,v1 = satimo.loadtrx("triag_top_horiz_hb.trx")
+f2,h2,v2 = satimo.loadtrx("triag_side_horiz_hb.trx")
+
+f_hb = f1
+ecc_hb = satimo.ecc(h1,h2,v1,v2)
+
+f_meas = hstack((f_lb, f_hb))
+ecc_meas = hstack((ecc_lb, ecc_hb))
+
+# Simulated
+f_sim,ecc_sim = loadtxt("sim/correlation.txt", skiprows=2).T
+
+aauplot.figure()
+aauplot.correlation(f_meas, ecc_meas, "-b", label="Measured")
+aauplot.correlation(f_sim, ecc_sim, "--b", label="Simulated")
+aauplot.end_correlation()
+
+savefig("correlation.pdf")
+
+show()
