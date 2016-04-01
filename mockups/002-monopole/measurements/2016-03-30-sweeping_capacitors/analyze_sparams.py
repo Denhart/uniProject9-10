c0111a7cd733879fa0204c53b0303fddbb8fa5f7
lt <lt@441b50e4-ef73-420c-861d-6e41df262e85>
2016-04-01 08:26:46 +0000

    SD

--- mockups/002-monopole/measurements/2016-03-30-sweeping_capacitors/analyze_sparams.py
+++ mockups/002-monopole/measurements/2016-03-30-sweeping_capacitors/analyze_sparams.py
@@ -21,6 +21,7 @@ side = ["lasse-side-0.3-pF.s2p",
         "lasse-side-3.0-pF.s2p",
         ]
 
+#S11
 aauplot.figure()
 for s in top:
     m = loadtxt(vnadir + s, skiprows=5).T
@@ -35,6 +36,37 @@ for s in top:
 aauplot.end_sparam()
 savefig("s11_csh1.pdf")
 
+#S21
+aauplot.figure()
+for s in top:
+    m = loadtxt(vnadir + s, skiprows=5).T
+    f = m[0]
+    S11 = m[1] + 1j*m[2]
+    S21 = m[3] + 1j*m[4]
+    S12 = m[5] + 1j*m[6]
+    S22 = m[7] + 1j*m[8]
+
+    aauplot.sparam(f, 20*log10(abs(S21)))
+
+aauplot.end_sparam()
+savefig("s21_csh1.pdf")
+
+#S12
+aauplot.figure()
+for s in top:
+    m = loadtxt(vnadir + s, skiprows=5).T
+    f = m[0]
+    S11 = m[1] + 1j*m[2]
+    S21 = m[3] + 1j*m[4]
+    S12 = m[5] + 1j*m[6]
+    S22 = m[7] + 1j*m[8]
+
+    aauplot.sparam(f, 20*log10(abs(S12)))
+
+aauplot.end_sparam()
+savefig("s12_csh1.pdf")
+
+#S22
 aauplot.figure()
 for s in side:
     m = loadtxt(vnadir + s, skiprows=5).T
