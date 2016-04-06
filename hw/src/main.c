ab5f95e2b51004c879a726ee387cb35c982826f5
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-04-06 12:51:01 +0000

    Faster 1.5 MHz implementation

--- hw/src/main.c
+++ hw/src/main.c
@@ -35,148 +35,173 @@ void usart0_init()
     sei();
 }
 
+// RFFE_XY, X=SCLK, Y=SDATA
+#define RFFE_HH 0b00000011
+#define RFFE_HL 0b00000010
+#define RFFE_LH 0b00000001
+#define RFFE_LL 0b00000000
+
 #define RFFE_PORT PORTC
 #define RFFE_SDATA 0
 #define RFFE_SCLK 1
 #define RFFE_DELAY_US 0
-static inline void rffe_sdata(uint8_t x)
-{
-    if (x)
-        RFFE_PORT |= (1 << RFFE_SDATA);
-    else
-        RFFE_PORT &= ~(1 << RFFE_SDATA);
-}
 
-static inline void rffe_sclk(uint8_t x)
-{
-    if (x)
-        RFFE_PORT |= (1 << RFFE_SCLK);
-    else
-        RFFE_PORT &= ~(1 << RFFE_SCLK);
-}
-
-static inline void rffe_start()
-{
-    // Start condition
-    rffe_sclk(0);
-    rffe_sdata(0);
-    _delay_us(RFFE_DELAY_US);
-
-    rffe_sdata(1);
-    _delay_us(RFFE_DELAY_US);
-
-    rffe_sdata(0);
-    _delay_us(RFFE_DELAY_US);
-}
-
-static inline void rffe_send_bit(uint8_t bit)
-{
-    rffe_sdata(bit);
-    rffe_sclk(1);
-    _delay_us(RFFE_DELAY_US);
-    rffe_sclk(0);
-    _delay_us(RFFE_DELAY_US);
-}
-
-static inline void rffe_send_slave_address(uint8_t address)
-{
-    int8_t i;
-    for (i = 3; i >= 0; i--){
-        rffe_send_bit((address >> i) & 0x01 );
-    }
-}
-
-static inline void rffe_send_parity(uint8_t p)
+uint8_t rffe_parity(uint8_t p)
 {
     p = p ^ (p >> 4 | p << 4);
     p = p ^ (p >> 2);
     p = p ^ (p >> 1);
     p &= 1;
 
-    rffe_send_bit(!p); // Total number of '1's == odd
-}
-
-static inline void rffe_send_byte(uint8_t byte)
-{
-    int8_t i;
-    // Clock out data
-    for (i = 7; i >= 0; i--){
-        rffe_send_bit((byte >> i) & 0x01 );
-    }
-
-    // Parity
-    rffe_send_parity(byte);
-}
-
-static inline void rffe_bus_park()
-{
-    rffe_send_bit(0);
+    return !p;
 }
 
 void rffe_set_reg(uint8_t address, uint8_t reg, uint8_t value)
 {
-    rffe_start();
-    rffe_send_slave_address(address);
-    rffe_send_byte((0x02 << 5) | reg); // Write reg cmd
-    rffe_send_byte(value);
-    rffe_bus_park();
+    uint8_t rffe_command[24];
+    uint8_t sclk_pin = (1 << RFFE_SCLK);
+    int8_t i, j;
+    uint8_t byte;
+
+    // 0:      Start: SDATA pulse while SCLK low
+    rffe_command[0] = RFFE_LH; 
+
+    // 1--4:   Address
+    for (i = 3, j = 1; i >= 0; i--, j++)
+        rffe_command[j] = ((address >> i) & 0x01) ? RFFE_HH : RFFE_HL;
+
+    // 5--12:  Write register command + register
+    byte = (0x02 << 5) | reg;
+    for (i = 7, j = 5; i >= 0; i--, j++)
+        rffe_command[j] = ((byte >> i) & 0x01) ? RFFE_HH : RFFE_HL;
+
+    // 13:     Parity
+    rffe_command[13] = rffe_parity(byte) ? RFFE_HH : RFFE_HL;
+
+    // 14--21: Value
+    for (i = 7, j = 14; i >= 0; i--, j++)
+        rffe_command[j] = ((value >> i) & 0x01) ? RFFE_HH : RFFE_HL;
+
+    // 22:     Parity
+    rffe_command[22] = rffe_parity(value) ? RFFE_HH : RFFE_HL;
+
+    // 23:     Bus park
+    rffe_command[23] = RFFE_HL;
+
+
+    PORTC = rffe_command[0]; // Start
+    PORTC = RFFE_LL;
+    PORTC = rffe_command[1]; // Slave address
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[2];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[3];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[4];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[5]; // Write Register Command
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[6];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[7];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[8]; // Register number
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[9];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[10];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[11];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[12];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[13]; // Parity
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[14]; // Value
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[15];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[16];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[17];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[18];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[19];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[20];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[21];
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[22]; // Parity
+    PORTC &= ~ sclk_pin;
+    PORTC = rffe_command[23]; // Bus park
+    PORTC &= ~ sclk_pin;
+
 }
 
 #define WS1040_A 0x07
 #define WS1040_B 0x06
+static uint8_t ws1040_address = WS1040_A;
 ISR (USART_RX_vect)
 {
     // Code to be executed when the USART receives a byte here
     static uint8_t r;
     r = UDR0;
     switch (r) {
+    case 'A':
+        ws1040_address = WS1040_A;
+        break;
+    case 'B':
+        ws1040_address = WS1040_B;
+        break;
     case '0':
-        rffe_set_reg(WS1040_A, 0x01, 0x00);
+        rffe_set_reg(ws1040_address, 0x01, 0x00);
         break;
     case '1':
-        rffe_set_reg(WS1040_A, 0x01, 0x01);
+        rffe_set_reg(ws1040_address, 0x01, 0x01);
         break;
     case '2':
-        rffe_set_reg(WS1040_A, 0x01, 0x02);
+        rffe_set_reg(ws1040_address, 0x01, 0x02);
         break;
     case '3':
-        rffe_set_reg(WS1040_A, 0x01, 0x03);
+        rffe_set_reg(ws1040_address, 0x01, 0x03);
         break;
     case '4':
-        rffe_set_reg(WS1040_A, 0x01, 0x04);
+        rffe_set_reg(ws1040_address, 0x01, 0x04);
         break;
     case '5':
-        rffe_set_reg(WS1040_A, 0x01, 0x05);
+        rffe_set_reg(ws1040_address, 0x01, 0x05);
         break;
     case '6':
-        rffe_set_reg(WS1040_A, 0x01, 0x06);
+        rffe_set_reg(ws1040_address, 0x01, 0x06);
         break;
     case '7':
-        rffe_set_reg(WS1040_A, 0x01, 0x07);
+        rffe_set_reg(ws1040_address, 0x01, 0x07);
         break;
     case '8':
-        rffe_set_reg(WS1040_A, 0x01, 0x08);
+        rffe_set_reg(ws1040_address, 0x01, 0x08);
         break;
     case '9':
-        rffe_set_reg(WS1040_A, 0x01, 0x09);
+        rffe_set_reg(ws1040_address, 0x01, 0x09);
         break;
     case 'a':
-        rffe_set_reg(WS1040_A, 0x01, 0x0a);
+        rffe_set_reg(ws1040_address, 0x01, 0x0a);
         break;
     case 'b':
-        rffe_set_reg(WS1040_A, 0x01, 0x0b);
+        rffe_set_reg(ws1040_address, 0x01, 0x0b);
         break;
     case 'c':
-        rffe_set_reg(WS1040_A, 0x01, 0x0c);
+        rffe_set_reg(ws1040_address, 0x01, 0x0c);
         break;
     case 'd':
-        rffe_set_reg(WS1040_A, 0x01, 0x0d);
+        rffe_set_reg(ws1040_address, 0x01, 0x0d);
         break;
     case 'e':
-        rffe_set_reg(WS1040_A, 0x01, 0x0e);
+        rffe_set_reg(ws1040_address, 0x01, 0x0e);
         break;
     case 'f':
-        rffe_set_reg(WS1040_A, 0x01, 0x0f);
+        rffe_set_reg(ws1040_address, 0x01, 0x0f);
         break;
     }
 }
