5fde1a8a4d3336618fc3d29cd459630c509caf57
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-04-07 19:26:52 +0000

    123

--- hw/src/writeflash.sh
+++ hw/src/writeflash.sh
@@ -1,1 +1,2 @@
-avrdude -c usbasp -p m328p -U flash:w:serial_rffe.hex
+# avrdude -c usbasp -p m328p -U flash:w:serial_rffe.hex
+avrdude -c usbasp -p m168p -U flash:w:serial_rffe.hex
--- hw/src/writefuses.sh
+++ hw/src/writefuses.sh
@@ -1,1 +1,1 @@
-avrdude -c usbasp -p m328p -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
+avrdude -c usbasp -p m168p -U lfuse:w:0xe2:m -U hfuse:w:0xd9:m -U efuse:w:0xff:m
