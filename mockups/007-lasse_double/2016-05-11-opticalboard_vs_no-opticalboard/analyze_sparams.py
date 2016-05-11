790516b6fcb4c608a2ece247f9e557ff0792812c
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-05-11 09:18:45 +0000

    123

---
+++ mockups/007-lasse_double/2016-05-11-opticalboard_vs_no-opticalboard/analyze_sparams.py
@@ -0,0 +1,25 @@
+from numpy import *
+from matplotlib.pyplot import *
+import aauplot
+
+aauplot.figure(figsize=(7,3))
+
+m = loadtxt("withboard_withbatteryCh1.csv", skiprows=3).T
+aauplot.sparam(m[0], m[1], '-b', label="S11 $+$ board $+$ bat")
+aauplot.sparam(m[0], m[2], '-g', label="S22 $+$ board $+$ bat")
+aauplot.sparam(m[0], m[3], '-r', label="S21 $+$ board $+$ bat")
+
+m = loadtxt("withboard_nobatteryCh1.csv", skiprows=3).T
+aauplot.sparam(m[0], m[1], '--b', label="S11 $+$ board $-$ bat")
+aauplot.sparam(m[0], m[2], '--g', label="S22 $+$ board $-$ bat")
+aauplot.sparam(m[0], m[3], '--r', label="S21 $+$ board $-$ bat")
+
+m = loadtxt("noboard_nobatteryCh1.csv", skiprows=3).T
+aauplot.sparam(m[0], m[1], '-.b', label="S11 $-$ board $-$ bat")
+aauplot.sparam(m[0], m[2], '-.g', label="S22 $-$ board $-$ bat")
+aauplot.sparam(m[0], m[3], '-.r', label="S21 $-$ board $-$ bat")
+
+aauplot.end_sparam(loc=8, ncol=3)
+savefig("sparam_comparison.pdf")
+show()
+# savefig("sparams_best_match.pdf")
