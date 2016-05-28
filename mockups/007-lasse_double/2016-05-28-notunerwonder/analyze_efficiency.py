6dfc07e4f9ee89b97fef9afc6dc3293ccb80030f
sn <sn@441b50e4-ef73-420c-861d-6e41df262e85>
2016-05-28 10:05:38 +0000

    Således

---
+++ mockups/007-lasse_double/2016-05-28-notunerwonder/analyze_efficiency.py
@@ -0,0 +1,52 @@
+from numpy import *
+from matplotlib.pyplot import *
+from glob import glob
+import aauplot
+import satimo
+
+reffiles = [
+        "satimo/ref/HomeRef600.ref",
+        "satimo/ref/SD740-70.ref",
+        "satimo/ref/SD850-02.ref",
+        "satimo/ref/SD900-51.ref",
+        "satimo/ref/SD1800-45.ref",
+        "satimo/ref/SD2050-36.ref",
+        "satimo/ref/SD2450-43.ref",
+        ]
+
+calfiles = [
+        "satimo/cal/665RefDipole.trx",
+        "satimo/cal/740RefDipole.trx",
+        "satimo/cal/850RefDipole.trx",
+        "satimo/cal/900RefDipole.trx",
+        "satimo/cal/1800RefDipole.trx",
+        "satimo/cal/2050RefDipole.trx",
+        "satimo/cal/2450RefDipole.trx",
+        ]
+
+f_tot,P_tot = satimo.totalpower_table(calfiles, reffiles)
+avg = 25
+aauplot.figure()
+
+# TOP ANTENNA ##################################################################
+print("Processing top antenna...")
+
+f_L,eff_L = satimo.efficiency("satimo/antenna/wonder-top-noTuner-LB-H.trx", f_tot=f_tot, P_tot=P_tot)
+f_H,eff_H = satimo.efficiency("satimo/antenna/wonder-top-noTuner-HB-H.trx", f_tot=f_tot, P_tot=P_tot)
+f = hstack((satimo.ma(f_L,avg),satimo.ma(f_H,avg)))
+eff = hstack((satimo.ma(eff_L,avg), satimo.ma(eff_H,avg)))
+aauplot.efficiency(f,eff, '-b', label="Top")
+
+# SIDE ANTENNA #################################################################
+print("Processing side antenna...")
+
+f_L,eff_L = satimo.efficiency("satimo/antenna/wonder-side-noTuner-LB-H.trx", f_tot=f_tot, P_tot=P_tot)
+f_H,eff_H = satimo.efficiency("satimo/antenna/wonder-side-noTuner-HB-H.trx", f_tot=f_tot, P_tot=P_tot)
+f = hstack((satimo.ma(f_L,avg),satimo.ma(f_H,avg)))
+eff = hstack((satimo.ma(eff_L,avg), satimo.ma(eff_H,avg)))
+aauplot.efficiency(f,eff, '-g', label="Side")
+
+aauplot.end_efficiency(loc=8, ncol=2);
+savefig("efficiency.pdf")
+
+show()
